24597138039da78e4e9346274454ca44e1f77210
Daniel Gaston <danielgaston6@gmail.com>
2020-01-08 17:03:09 -0500

    Initial Commits to Github

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Greatness Starts Here !!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
